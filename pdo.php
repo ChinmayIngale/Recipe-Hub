a203ce0e20b20b33b657f38d2b0bac759f98bd41
Chinmay Ingale <64984975+ChinmayIngale@users.noreply.github.com>
2021-08-24 16:29:25 +0530

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,6 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=epiz_27491132_recipes','root', '');
-// $pdo = new PDO('mysql:host=sql104.epizy.com;dbname=epiz_27491132_recipes','epiz_27491132', 'fw8yo4VcfpO');
+$pdo = new PDO('mysql:host=HOST_NAME;dbname=DATABASE_NAME','USERNAME', 'PASSWORD');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
